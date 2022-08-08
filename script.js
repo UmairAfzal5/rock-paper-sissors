c33b98c14c6217dceeccd9707daf6c5e465bfe01
Umair Afzal <Umairafzal545@outlook.com>
2022-08-08 12:36:59 -0700

    made the playRound function and getComputerChoice Function

--- script.js
+++ script.js
@@ -1,1 +1,37 @@
-console.log("hello world");
+//function that will return rock paper or sissor for the AI
+//function that takes in the computer selection and playerSelection then return a string to declare who lost
+
+function getComputerChoice(){
+    let random = Math.floor(Math.random()* 3)
+
+
+    if(random === 0)
+        return 'rock';
+    else if(random === 1)
+        return 'paper';
+    else
+        return 'sissors';
+}
+
+function playRound (playerSelection, computerSelection){
+    if(playerSelection === computerSelection)
+        return "Tie!"
+    else if(playerSelection === "rock" && computerSelection === "sissors")
+        return "You Win! Rock beats Sissors";
+    else if(playerSelection === "rock" && computerSelection === "paper")
+        return "You Lose! Paper beats Rock";
+    else if(playerSelection === "sissors" && computerSelection === "paper")
+        return "You Win! Sissors beats paper";
+    else if(playerSelection === "sissors" && computerSelection === "rock")
+        return "You Lose! Rock beats Sissors";
+    else if(playerSelection === "paper" && computerSelection === "rock")
+        return "You Win! Paper beats Rock";
+    else 
+        return "you Lose! Sissors beats paper!";
+}
+
+const playerSelection = "paper";
+let computerSelection = getComputerChoice();
+
+console.log(playRound(playerSelection, computerSelection));
+ console.log(computerSelection);
